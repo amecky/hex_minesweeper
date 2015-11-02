510ae9f4d299e60f3951bdba017aab762bfd8b4b
Andreas Mecky <a.mecky@verband.creditreform.de>
2015-11-02 15:58:20 +0100

    more new layout testing

--- src/gamestates/TestGameState.cpp
+++ src/gamestates/TestGameState.cpp
@@ -15,14 +15,14 @@ TestGameState::~TestGameState() {
 // activate
 // --------------------------------------------
 void TestGameState::activate() {
-	_gui->activate("Test");
+	//_gui->activate("Test");
 }
 
 // --------------------------------------------
 // activate
 // --------------------------------------------
 void TestGameState::deactivate() {
-	_gui->deactivate("Test");
+	//_gui->deactivate("Test");
 }
 
 // --------------------------------------------
@@ -43,14 +43,17 @@ int TestGameState::onGUIButton(ds::DialogID dlgID, int button) {
 // render
 // --------------------------------------------
 void TestGameState::render() {
+	int iv = 100;
 	ds::sprites::draw(v2(512, 384), ds::math::buildTexture(ds::Rect(0, 512, 512, 384)), 0.0f, 2.0f, 2.0f);
 	ds::sprites::drawTiledXY(v2(100, 384), v2(240, 120), ds::math::buildTexture(760.0f, 160.0f, 100.0f, 60.0f),10.0f);
 	ds::sprites::drawTiledXY(v2(100, 584), v2(240.0f,140.f), ds::math::buildTexture(760.0f, 270.0f, 100.0f, 100.0f), 10.0f);
+	ds::sprites::drawTiledX(v2(150,540), 80.0f, ds::math::buildTexture(860.0f, 0.0f, 150.0f, 24.0f),24.0f);
 	int state = 1;
 	v2 pos(512, 450);
 	gui::start(1, &pos);
 	if (gui::begin("Test", &state)) {
 		//gui::BoxTest();
+		gui::InputInt(4, "Int Value", &iv);
 		gui::beginGroup();
 		if (gui::Button(2, "OK")) {
 			LOG << "OK pressed";
