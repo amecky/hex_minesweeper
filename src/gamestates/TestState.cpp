e1a94fd23be202727a6dc3689955ca2c59b50305
meckya <amecky@gmail.com>
2016-04-28 21:32:43 +0200

    working on particles

--- src/gamestates/TestState.cpp
+++ src/gamestates/TestState.cpp
@@ -24,6 +24,16 @@ void TestState::render() {
 }
 
 int TestState::onButtonUp(int button, int x, int y) {
+	/*
+	ds::ColorModuleData* cm = (ds::ColorModuleData*)_particles->getParticleSystem(1)->getData(ds::PM_COLOR);
+	cm->color = ds::Color(192, 0, 0, 255);
+	cm = (ds::ColorModuleData*)_particles->getParticleSystem(2)->getData(ds::PM_COLOR);
+	cm->color = ds::Color(255, 0, 0, 255);
+	cm = (ds::ColorModuleData*)_particles->getParticleSystem(3)->getData(ds::PM_COLOR);
+	cm->color = ds::Color(255, 64, 0, 255);
+	*/
 	_particles->start(1, v2(x, y));
+	_particles->start(2, v2(x, y));
+	_particles->start(3, v2(x, y));
 	return 0;
 }
