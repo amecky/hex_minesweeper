fc54b231b7de9354fe8a846bb8216f4cd628f8a1
meckya <amecky@gmail.com>
2016-07-17 21:49:48 +0200

    fixing bugs

--- src/gamestates/MainMenuState.h
+++ src/gamestates/MainMenuState.h
@@ -8,6 +8,13 @@ public:
 	MainMenuState(GameContext* context, ds::Game* game);
 	~MainMenuState();
 	int onGUIButton(int button);
+	int update(float dt) {
+		_dialog->tick(dt);
+		return 0;
+	}
+	void activate() {
+		_dialog->setNumber(7, 4567);
+	}
 private:
 	GameContext* _context;
 };
