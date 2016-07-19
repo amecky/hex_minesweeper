1f5326f3caa65731442a44635d29ffa1ea4e328e
Andreas Mecky <a.mecky@verband.creditreform.de>
2016-07-19 15:51:25 +0200

    adding transition effect

--- src/gamestates/MainMenuState.h
+++ src/gamestates/MainMenuState.h
@@ -14,7 +14,11 @@ public:
 	}
 	void activate() {
 		_dialog->setNumber(7, 4567);
-		_dialog->startTransition(4, v2(-200, 100), 1.0f);
+		_dialog->startTransition(1, v2(1200, 500), 1.0f);
+		_dialog->startTransition(2, v2(-200, 400), 1.0f);
+		_dialog->startTransition(3, v2(1200, 300), 1.0f);
+		_dialog->startTransition(5, v2(-200, 200), 1.0f);
+		_dialog->startTransition(4, v2(1200, 100), 1.0f);
 	}
 private:
 	GameContext* _context;
